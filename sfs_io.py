8263ead5d22563403b309da788ec457a0c1a84ae
Patrick Snape <patricksnape@gmail.com>
2014-09-05 17:38:32 +0100

    Fixing for latest menpo changes

--- sfs_io.py
+++ sfs_io.py
@@ -1,16 +1,72 @@
 import os.path
 import cPickle
 import menpo.io as mio
-from inpaint import replace_nans
-from scipy.ndimage.filters import gaussian_filter, median_filter
+from scipy.ndimage.filters import median_filter
 import numpy as np
 import sys
+import cv2
 
 
 def print_replace_line(string):
     # Cheeky carriage return so we print on the same line
     sys.stdout.write('\r' + string)
     sys.stdout.flush()
+
+
+def rescale(val, in_min, in_max, out_min, out_max):
+    return out_min + (val - in_min) * ((out_max - out_min) / (in_max - in_min))
+
+
+def rescale_to_opencv_image(depth_image):
+    nan_max = np.nanmax(depth_image)
+    nan_min = np.nanmin(depth_image)
+    depth_image[np.isnan(depth_image)] = nan_min - 2
+    rescaled_depth = rescale(depth_image,
+                             np.nanmin(depth_image), nan_max,
+                             0.0, 1.0)
+    return (rescaled_depth * 255).astype(np.uint8)
+
+
+def rescale_to_depth_image(original_image, opencv_image):
+    nan_max = np.nanmax(original_image.pixels[..., 2])
+    nan_min = np.nanmin(original_image.pixels[..., 2])
+    depth_pixels = opencv_image.astype(np.float) / 255.0
+    depth_pixels = rescale(depth_pixels, 0.0, 1.0, nan_min, nan_max)
+    depth_pixels[np.isclose(np.nanmin(depth_pixels), depth_pixels)] = np.nan
+    return depth_pixels
+
+
+def preprocess_image(image):
+    # Copy the depth part of the image
+    depth_pixels = image.pixels[..., 2].copy()
+    depth_pixels = rescale_to_opencv_image(depth_pixels)
+    filtered_depth_pixels = median_filter(depth_pixels, 5)
+
+    # Build mask for floodfilling, this lets me ignore all the pixels
+    # from the background and around the ears
+    mask = np.zeros((depth_pixels.shape[0] + 2, depth_pixels.shape[1] + 2),
+                    dtype=np.uint8)
+    # Flood fill from top left
+    cv2.floodFill(filtered_depth_pixels, mask, (0, 0),
+                  (255, 255, 255), flags=cv2.FLOODFILL_MASK_ONLY)
+    # Flood fill from top right
+    cv2.floodFill(filtered_depth_pixels, mask, (depth_pixels.shape[1] - 1, 0),
+                  (255, 255, 255), flags=cv2.FLOODFILL_MASK_ONLY)
+    # Truncate and negate the flood filled areas to find the facial region
+    floodfill_mask = (~mask.astype(np.bool))[1:-1, 1:-1]
+
+    # Build a mask of the areas inside the face that need inpainting
+    inpaint_mask = ~image.mask.mask & floodfill_mask
+    # Inpaint the image and filter to smooth
+    inpainted_pixels = cv2.inpaint(depth_pixels,
+                                   inpaint_mask.astype(np.uint8),
+                                   5, cv2.INPAINT_NS)
+    inpainted_pixels = median_filter(inpainted_pixels, 5)
+
+    # Back to depth pixels
+    image.pixels[..., 2] = rescale_to_depth_image(image, inpainted_pixels)
+    # Reset the mask!
+    image.mask.pixels[..., 0] = ~np.isnan(image.pixels[..., 2])
 
 
 def load_frgc(session_id, recreate_meshes=False,
@@ -32,15 +88,13 @@ def load_frgc(session_id, recreate_meshes=False,
             len(images) / float(len(all_images)) * 100)
 
         for i, im in enumerate(images):
-            im.pixels[..., 2] = replace_nans(im.pixels[..., 2], 100, 0.1)
-            im.pixels[..., 2] = median_filter(im.pixels[..., 2], 5.0)
-            im.pixels[..., 2] = gaussian_filter(im.pixels[..., 2], [2.0, 3.0])
-            im.rebuild_mesh()
+            preprocess_image(im)
             print_replace_line(
                 'Image {0} of {1} cleaned'.format(i + 1, len(images)))
         # Only dump the saved images if we loaded all of them!
         if max_images is None:
-            cPickle.dump(images, open(previously_pickled_path, 'wb'), protocol=2)
+            cPickle.dump(images, open(previously_pickled_path, 'wb'),
+                         protocol=2)
 
     return images
 
